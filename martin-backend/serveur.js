1dec1a7705bcf33eb257ca044ecf9667abf61b80
noah bernardi <noah.bernardi12@gmail.com>
2026-05-05 21:26:09 +0200

    modification serveur.js

--- martin-backend/serveur.js
+++ martin-backend/serveur.js
@@ -20,7 +20,9 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err)=>console.log(err))
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
