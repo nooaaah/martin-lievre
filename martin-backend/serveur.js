ecce46f2623ddd86ba973bd023bf71772b2d9ec3
noah bernardi <noah.bernardi12@gmail.com>
2026-05-05 21:22:53 +0200

    modification serveur.js

--- martin-backend/serveur.js
+++ martin-backend/serveur.js
@@ -8,7 +8,11 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://martin-lievre.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
