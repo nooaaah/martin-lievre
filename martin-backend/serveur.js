08bd293ca7b4e84c4a7d9bd3079be5219631c4d1
noah bernardi <noah.bernardi12@gmail.com>
2026-05-20 13:10:32 +0200

    changement route contact

--- martin-backend/serveur.js
+++ martin-backend/serveur.js
@@ -98,7 +98,7 @@ app.post("/contact", async (req, res) => {
     const resend = new Resend(process.env.RESEND_API_KEY)
     resend.emails.send({
       from: "onboarding@resend.dev",
-      to: "delivered@resend.dev",
+      to: process.env.EMAIL_TO,
       subject: `Nouveau message de ${nom}`,
       text: `Nom: ${nom}\nEmail: ${email}\n\nMessage:\n${message}`
     }).then(data => console.log("email envoyé:", data))
